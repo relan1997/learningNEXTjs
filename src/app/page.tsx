3d77897b0ab77f5b4b0b3eca3d921bc9583cfbf7
relan1997 <harshal.relan.201116@gmail.com>
2024-03-13 23:33:23 +0530

    Added the Cards which need slight deBugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import HeroSection from "@/components/HeroSection";
-
+import FeaturedCourses from "@/components/FeaturedCourses";
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-     
-     <HeroSection/>
+     <HeroSection/>  {/* these are the children that will be passed into the layout and get set exactly the way they are stacked in page */}
+     <FeaturedCourses/>
    </main>
   );
 }
