8f1d616fd3fa1d354a5cfb8ab58395d4a4740910
relan1997 <harshal.relan.201116@gmail.com>
2024-03-21 11:41:51 +0530

    added the card hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUse from "@/components/WhyChooseUs";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -9,6 +10,7 @@ export default function Home() {
      <FeaturedCourses/>
      <WhyChooseUse/>  {/* all the components that are not to displayed on every route will be added to the page and also the components that will not be part of every section and need a structured layout will also be needed to be added in page */}
     <TestimonialCards/>
+    <UpcomingWebinars/>
    </main>
   );
 }
