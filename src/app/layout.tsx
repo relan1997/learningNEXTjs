200f1bd31b5ae0414d6104657e98e7f3b0dd1ab8
relan1997 <harshal.relan.201116@gmail.com>
2024-06-24 15:38:04 +0530

    started work on next again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{ // tells that children that is recieved is read only and cannot be altered
   children: React.ReactNode; // this specifically specifies the type that the children will have which is a react component 
 }>) {
